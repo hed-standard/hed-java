5ae8867fd72728626521bdd2c440e78bf47aabe3
Dung Truong <dt.young112@gmail.com>
2020-02-21 17:30:06 -0800

    Remove field generator

--- java/field-generator/src/FieldGeneratorLauncher.kt
+++
@@ -1,5 +0,0 @@
-import tornadofx.launch
-
-fun main(args: Array<String>) {
-    launch<FieldGenerator>("{\"newFieldName\": \"trial_type\", \"eventFields\": {\"type\": [\"square\", \"rt\"], \"position\": [\"1\", \"2\"], \"test\": [\"square\", \"rt\"], \"test1\": [\"1\", \"2\"]}}")
-}
